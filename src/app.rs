a856d0918ffad47668fad5520dcd00a9bb174903
Your Name <you@example.com>
2024-03-24 12:25:03 +0200

    testing stuff

--- src/app.rs
+++ src/app.rs
@@ -1,52 +1,101 @@
 use leptos::*;
 use leptos_meta::*;
+use leptos_router::*;
 
-fn resize(imgs: &[(usize, RwSignal<usize>)]) {
-    for img in imgs {
-        img.1.update(|i| *i += 1);
+#[derive(Debug, Clone)]
+struct Img {
+    id: usize,
+    data: RwSignal<usize>,
+}
+
+impl Img {
+    pub fn new(id: usize, data: usize) -> Self {
+        Self {
+            id,
+            data: RwSignal::new(data)
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+struct GlobalState {
+    imgs: RwSignal<Vec<Img>>
+}
+
+impl GlobalState {
+    pub fn new() -> Self {
+        GlobalState {
+            imgs: RwSignal::new(Vec::new())
+        }
     }
 }
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
-    provide_context(RwSignal::new(vec![(0usize, RwSignal::new(0usize))]));
-    let global_imgs = use_context::<RwSignal<Vec<(usize, RwSignal<usize>)>>>().unwrap();
-    let received_imgs = RwSignal::new(0usize);
-
-    create_effect(move |_| {
-        received_imgs.with(move |received_imgs| {
-            let mut new_imgs: Vec<(usize, RwSignal<usize>)> = Vec::new();
-            let len = global_imgs.with(|i| i.len());
-            for i in len..len + received_imgs {
-                new_imgs.push((i, RwSignal::new(len)))
-            }
-            global_imgs.update(move |imgs| {
-                imgs.extend(new_imgs);
-                resize(imgs);
-            });
-        });
-    });
-
-    let on_add = move |_| {
-        received_imgs.set(25);
-    };
-    let on_inc = move |_| {
-        global_imgs.with(|imgs| {
-            resize(imgs);
-        })
-    };
+    provide_context(GlobalState::new());
 
     view! {
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+        <a href="/" >page one</a>
+        <br/>
+        <a href="/two" >page two</a>
+        <br/>
+        <Router>
+        <Routes>
+                <Route path="" view=PageOne/>
+                <Route path="/two" view=PageTwo/>
+            </Routes>
+    </Router>
+    }
+}
+
+#[component]
+fn PageOne() -> impl IntoView {
+    let global_state = use_context::<GlobalState>().unwrap();
+
+    let on_add = move |_| {
+        global_state.imgs.update(|imgs| {
+            imgs.push(Img::new(imgs.len(),0));
+        });
+    };
+
+    let on_inc = move |_| {
+        global_state.imgs.with(|imgs| {
+            for img in imgs {
+                img.data.update(|data| *data += 1);
+            }
+        });
+    };
+
+    view! {
         <button on:click=on_add>"ADD"</button>
         <button on:click=on_inc>"INCREMENT"</button>
+        <h1>"PAGE ONE"</h1>
         <ul>
-            <For each=global_imgs key=|img|img.0 let:img>
+            <For each=global_state.imgs key=|img|img.id let:img>
                 <li>{
-                    let a = img.1;
                     view!{
-                        <span>{move || a.get()}</span>
+                        <span>{move || img.data.get()}</span>
+                    }
+                }</li>
+            </For>
+        </ul>
+    }
+}
+
+#[component]
+fn PageTwo() -> impl IntoView {
+    let global_state = use_context::<GlobalState>().unwrap();
+
+    view! {
+        <h1>"PAGE TWO"</h1>
+        <ul>
+            <For each=global_state.imgs key=|img|img.id let:img>
+                <li>{
+                    view!{
+                        <span>{move || img.data.get()}</span>
                     }
                 }</li>
             </For>
